9a04c1868a5970effb3157dd093e34e708f0ed4b
hazao <hazao@DESKTOP-D7KNJRN>
2016-08-05 13:40:00 +0100

    Implemented "exit" to stop currently running program - program itself not working now.

--- SimpleColour/SimpleColour/SimpleColour.ino
+++ SimpleColour/SimpleColour/SimpleColour.ino
@@ -1,8 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
 #define NUM_LEDS 300
+char R;
+String end;
+int n, i, G, B, program, stop = 0, colour[3];
 
-int n, i, R, G, B, program, stop = 0, colour[3];
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
@@ -14,23 +16,7 @@ void setup() {
 		strip.setPixelColor(n, 0, 0, 100);
 	}
 	strip.show(); // Initialize all pixels to 'off'
-	Serial.println("Available programs:");
-	Serial.println("1. Simple Colour Changer");
-	Serial.println("2. Flash");
-	Serial.println("3. something else");
-	Serial.println("Please select desired program: ");
-	while (Serial.available() == 0)
-	{
-
-		program = Serial.parseInt();
-
-		if (program > 0)
-		{
-			Serial.print("You selected program ");
-			Serial.println(program);
-			break;
-		}
-	}
+	chooseProgram();
 }
 
 void loop() {
@@ -38,25 +24,45 @@ void loop() {
 	switch (program)
 	{
 	case 1:
+		stop = 0;
 		Serial.println("\n\n********** Simple Colour Changer **********");
 		while (stop == 0)
 		{
 			if (Serial.available()) {
 				for (i = 0; i < 3; i++)
 				{
-					colour[i] = Serial.parseInt();
-					//R = R - '0';
-					// say what you got:
-					Serial.print("I received: ");
-					Serial.println(colour[i], DEC);
+					//colour[i] = Serial.parseInt();
+					//R = Serial.read();
+					end = Serial.readStringUntil('\n');
+					//exit.concat(R);
+					if (end == "exit")
+					{
+						stop = 1;
+						break;
+					}
+					else
+					{
+
+						//R = R - '0';
+						// say what you got:
+						Serial.print("I received: ");
+						Serial.println(colour[i], DEC);
+					}
 				}
 				changeColour(colour);
 			}
 		}
+		chooseProgram();
+
 		break;
 	default:
-		Serial.println("Error: Invalid number entered. Exiting...");
-		return;
+		Serial.println("Error: Invalid number entered. Please reselect...");
+		delay(500);
+		chooseProgram();
+		break;
+
+	case 2:
+		Serial.println("Nothing to see here...");
 		break;
 	}
 }
@@ -81,3 +87,25 @@ if (Serial.available() > 0) {
 	Serial.print("I received: ");
 	Serial.println(incomingByte, DEC);
 } */
+void chooseProgram()
+{
+	Serial.println("Available programs:");
+	Serial.println("1. Simple Colour Changer");
+	Serial.println("2. Flash");
+	Serial.println("3. something else");
+	Serial.println("Please select desired program: ");
+	while (Serial.available() == 0)
+	{
+
+		program = Serial.parseInt();
+
+		if (program > 0)
+		{
+			Serial.print("Launching Program ");
+			Serial.print(program);
+			Serial.print("...");
+			delay(2000);
+			break;
+		}
+	}
+}
