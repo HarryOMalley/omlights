e33db7f23d43b97db862ead75362d4ac711421f1
Harry O'Malley <hazaomalley@gmail.com>
2019-02-11 01:13:08 +0000

    Started working on making a FastLED based controller

--- Controller/Controller/__vm/.Controller.vsarduino.h
+++ Controller/Controller/__vm/.Controller.vsarduino.h
@@ -1,5 +1,5 @@
 /* 
-	Editor: http://www.visualmicro.com
+	Editor: https://www.visualmicro.com/
 			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
 			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
@@ -16,7 +16,7 @@
 #define __AVR_ATmega328P__
 #define _VMDEBUG 1
 #define F_CPU 16000000L
-#define ARDUINO 10803
+#define ARDUINO 10807
 #define ARDUINO_AVR_UNO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
@@ -27,9 +27,6 @@
 #define __inline__
 #define __volatile__
 #define GCC_VERSION 40902
-
-#define __cplusplus 201103L
-#undef __cplusplus
 #define __cplusplus 201103L
 
 #define volatile(va_arg) 
@@ -63,13 +60,15 @@ typedef void *__builtin_va_list;
 
 
 
-#include <Arduino.h>
+#include <arduino.h>
 #include <pins_arduino.h> 
-#undef F
-#define F(string_literal) ((const PROGMEM char *)(string_literal))
+//#undef F
+//#define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
 
+//typedef unsigned char uint8_t;
+//typedef unsigned int uint8_t;
 
 #define pgm_read_byte(address_short) uint8_t() 
 #define pgm_read_word(address_short) uint16_t() 
